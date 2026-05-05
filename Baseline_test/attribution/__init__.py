9172fb15fcc5ead4617c3771adcaee6485e7143c
zhangxiaolei.128 <zhangxiaolei.128@jd.com>
2026-05-05 23:30:46 +0800

    第一版03

--- Baseline_test/attribution/__init__.py
+++ Baseline_test/attribution/__init__.py
@@ -1,3 +1,6 @@
+from .analyzer import ECAPAAttributionAnalyzer
+from .integrated_gradients import IntegratedGradients_ECAPA
+from .baseline import BaselineComputer
 from .reliability import (
     deletion_insertion_test,
     batch_deletion_insertion_test,
